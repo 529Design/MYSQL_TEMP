b4cc624d89b78c97f0d8f2d9be9afc9e2e3c3233
JMAR <529Design@users.noreply.github.com>
2018-04-26 09:17:48 -0400

    Update AES_TESTING.sql

--- AES_TESTING.sql
+++ AES_TESTING.sql
@@ -1,4 +1,4 @@
---This creates a new table from an existing one
+/*This creates a new table from an existing one*/
 
 CREATE TABLE test_table
 (
@@ -10,3 +10,18 @@ primary key (id)
 );
 
 insert into test_table (first_name, last_name, email) (select first_name, last_name, email from other_table);
+
+/*Sets a variable in case of many fields to encrypt with same key*/
+
+SET @skeletonkey = 'mypassword'
+
+/*casts to binary*/
+
+alter TABLE test_table MODIFT OLUMN id varbinary(255);
+
+/* adds encryption */
+UPDATE test_table SET id = AES_ENCRYPT(id,@skeletonkey);
+
+SELECT cast(AES_DECRYPT(id,@skeletonkey) as char(255)) from test_table
+
+/* or the select can contain the plain text key ''mypassword' */
